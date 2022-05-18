ca1367ba1e7191a6d16ca398d309f6d0d9391de8
Acer <kulna>
2022-05-18 16:28:37 +0600

    init: start home work!

---
+++ script.js
@@ -0,0 +1,32 @@
+////Задача #1///
+
+/*
+let num = 10;
+let result = num++;
+console.log(num);
+console.log(result);
+*/
+
+/*
+let num = 10;
+let result = ++num;
+console.log(num);
+console.log(result);
+*/
+
+/*let num = 10;
+let result = num--;
+console.log(num);
+console.log(result);*/
+
+
+////Задача #2///
+
+let a = 17;
+let b = 10;
+let d = 7;
+const c = a - b;
+console.log(c);
+const result = c + d;
+console.log(result);
+
